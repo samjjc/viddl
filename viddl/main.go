1154b21eef7d214411cf1915508658953b7b1ab8
samjjc <sam111jc@gmail.com>
2018-05-21 15:32:04 -0400

    playlists and single video cli works

--- viddl/main.go
+++ viddl/main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
+	"flag"
+	"os"
+	"path/filepath"
 	"time"
 
 	. "github.com/samjjc/viddl"
+	"github.com/vbauerster/mpb"
 )
 
 func main() {
-	// flag.Parse()
-	// log.Println(flag.Args())
-	// currentDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-	// log.Println("download to dir=", currentDir)
-	// y := NewYoutube(true)
-	// arg := flag.Arg(0)
+	isPlaylist := flag.Bool("p", false, "link of youtube playlist instead of single video")
+	flag.Parse()
 
-	// y.DecodeURL(arg)
-	// y.StartDownload(currentDir)
-	DownloadPlaylist()
+	currentDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	// url := "https://www.youtube.com/watch?v=PyIMjnSfTKc&list=PLDwdWAXmoUpii8stEkuKPguv4SHhWcHQe&t=0s&index=2"
+	// url := "https://www.youtube.com/playlist?list=PLDwdWAXmoUpii8stEkuKPguv4SHhWcHQe"
+	url := flag.Arg(0)
+
+	if *isPlaylist {
+
+		DownloadPlaylist(currentDir, url)
+
+	} else {
+
+		p := mpb.New()
+		bar := CreateLoadingBar(p)
+		y := NewYoutube(false, bar)
+
+		y.DownloadSingleVideo(currentDir, url)
+	}
+
+	//sleep to finish drawing loading bar
 	time.Sleep(time.Millisecond * 200)
 }
